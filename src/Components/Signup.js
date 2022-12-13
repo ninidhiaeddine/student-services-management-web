4ce5cb0389d8ab5062677eb3176550a653697ae2
Omar <Obissani909@gmail.com>
2022-12-13 23:00:20 +0200

    First frontend commit: student signup, signin, and main page

---
+++ src/Components/Signup.js
@@ -0,0 +1,221 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+import './Mainpage.css'
+const theme = createTheme();
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+
+export default function SignUp() {
+    //password
+    const [values, setValues] = React.useState({
+        showPassword: false,
+      });
+    
+    const handleChangePassword = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+    
+      const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+    
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+    
+      //Dropdown 
+      const [Gender, setGender] = React.useState('');
+      const [open, setOpen] = React.useState(false);
+
+        const handleChangeGender = (event) => {
+            setGender(event.target.value);
+        };
+        const handleClose = () => {
+            setOpen(false);
+          };
+        
+          const handleOpen = () => {
+            setOpen(true);
+          };
+        
+
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate 
+          //onSubmit={handleSubmit} 
+          sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="IdNumbers"
+                  required
+                  fullWidth
+                  id="IdNumbers"
+                  label="ID Number"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} >
+                <FormControl sx={{ minWidth: '100%'}}>
+                <InputLabel id="GenderDropDownLabel">Gender</InputLabel>
+                 <Select
+                    labelId="GenderDropDownLabel"
+                    id="GenderDropDown"
+                    open={open}
+                    onClose={handleClose}
+                    onOpen={handleOpen}
+                    value={Gender}
+                    label="Gender"
+                    onChange={handleChangeGender}
+                    >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={1}>Male</MenuItem>
+                <MenuItem value={2}>Female</MenuItem>
+                <MenuItem value={3}>Other</MenuItem>
+                </Select>   
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChangePassword('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Confirm Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+                </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="DormStudent" color="primary" />}
+                  label="Dorm Student?"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIN from './Components/SignInPage.js';
+import Main from './Components/MainPage.js'
+import Test from './Components/test.js'
+import SignUp from './Components/StudentSignup.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//images
+//import logo from './Picture1.png';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Main/>
+  //<SignIN/>
+  //<SignUp/>
+  //<Test/>
+); 
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
