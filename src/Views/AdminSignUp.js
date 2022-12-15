86bd0374d1acb00f238cecf39837377ae6c44c37
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-15 02:35:42 +0200

    Removed unnecessary imports

--- src/Views/AdminSignUp.js
+++ src/Views/AdminSignUp.js
@@ -3,7 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
