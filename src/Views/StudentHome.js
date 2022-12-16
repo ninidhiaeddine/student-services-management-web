bad64c1cebcc6c6927e0bc8cf146e2a6b82b93c8
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-16 04:05:40 +0200

    Renamed StudentCalendar to StudentHome and added date picker

---
+++ src/Views/StudentHome.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { useState } from 'react';
+import TimeSlot from '../Components/TimeSlot';
+import Grid from '@mui/material/Grid';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+//icons
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import PoolIcon from '@mui/icons-material/Pool';
+import DryCleaningIcon from '@mui/icons-material/DryCleaning';
+import LocalLaundryServiceIcon from '@mui/icons-material/LocalLaundryService';
+
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#26D1B3',
+    },
+    mode: 'dark',
+  },
+});
+
+
+export default function StudentHome() {
+  const [value, setValue] = useState([null, null]);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <LocalizationProvider sx={{minWidth: 200}} dateAdapter={AdapterDayjs}>
+        <StaticDatePicker
+          displayStaticWrapperAs="desktop"
+          openTo="day"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+            console.log(newValue.$D);
+            console.log(newValue.$M + 1);
+            console.log(newValue.$y);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+
+      <Container>
+      <Grid container columnSpacing={30} rowSpacing={2}>
+      </Grid>
+      </Container>
+      
+    </ThemeProvider>
+  );
+}
