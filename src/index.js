e66c5833778d4ddcd2acb6ef2a8a32db0ad67204
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-16 05:14:22 +0200

    Moved all app logic to App.js instead on index.js

--- src/index.js
+++ src/index.js
@@ -3,44 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import SignIn from './Views/SignIn.js';
-import Home from './Views/Home.js'
-import StudentSignUp from './Views/StudentSignup.js'
-import StudentHome from './Views/StudentHome.js'
-import AdminSignUp from './Views/AdminSignUp.js';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "/SignIn",
-    element: <SignIn/>,
-  },
-  {
-    path: "/StudentSignUp",
-    element: <StudentSignUp/>,
-  },
-  {
-    path: "/AdminSignUp",
-    element: <AdminSignUp/>,
-  },
-  {
-    path: "/StudentHome",
-    element: <StudentHome/>,
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-); 
+root.render(<App/>); 
 
 
 // If you want to start measuring performance in your app, pass a function
