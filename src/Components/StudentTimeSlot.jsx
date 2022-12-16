cf5503fe24ddd5c9d51b63b4a09f1687c99148f6
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-16 13:07:55 +0200

    Added Dialog for StudentTimeSlot

--- src/Components/StudentTimeSlot.jsx
+++ src/Components/StudentTimeSlot.jsx
@@ -4,9 +4,17 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useEffect, useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Grid from '@mui/material/Grid';
 
-export default function TimeSlot(props) {
+export default function StudentTimeSlot(props) {
     const [isAvailable, setIsAvailable] = useState(true);
+    const [open, setOpen] = useState(false);
+
     useEffect(() => {
         let condition = props.currentCapacity === props.maxCapacity;
         setIsAvailable(!condition);
@@ -28,26 +36,73 @@ export default function TimeSlot(props) {
             </Typography>)
     }
 
-    return (
-        <Card sx={{ minWidth: 280, padding: 1 }}>
-            <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    Time Slot
-                </Typography>
-                <Typography variant="h5" component="div">
-                    {props.startTime} - {props.endTime}
-                </Typography>
-                <Typography variant="body2">
-                    Capacity: {props.currentCapacity} / {props.maxCapacity}
-                </Typography>
-                <AvailableTypography isAvailable={isAvailable} />
+    function handleBookNow(event) {
+        setOpen(true);
+    }
 
-            </CardContent>
-            <CardActions>
-                <Button disabled={!isAvailable} variant="contained">
-                    {isAvailable ? "Book Now" : "Fully booked :("}
-                </Button>
-            </CardActions>
-        </Card>
+    return (
+        <div>
+            <Card sx={{ minWidth: 280, padding: 1 }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Time Slot
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        {props.startTime} - {props.endTime}
+                    </Typography>
+                    <Typography variant="body2">
+                        Capacity: {props.currentCapacity} / {props.maxCapacity}
+                    </Typography>
+                    <AvailableTypography isAvailable={isAvailable} />
+
+                </CardContent>
+                <CardActions>
+                    <Button disabled={!isAvailable} variant="contained" onClick={handleBookNow}>
+                        {isAvailable ? "Book Now" : "Fully booked :("}
+                    </Button>
+                </CardActions>
+            </Card>
+
+            {/* dialog */}
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Confirm Booking"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are you sure you would like to confirm the following booking?
+                        <Grid container sx={{marginTop: 3}}>
+                            <Grid item xs={6}>
+                                <p>Time Slot: </p>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <p style={{fontWeight: 'bold'}}>{props.startTime} - {props.endTime}</p>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <p>Service: </p>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <p style={{fontWeight: 'bold'}}>{props.serviceType}</p>
+                            </Grid>
+                        </Grid>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>Cancel</Button>
+                    <Button onClick={() => {
+                        setOpen(false);
+                        props.bookingCallback(props.index);
+                    }} autoFocus>
+                        Confirm
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+
     );
 }
