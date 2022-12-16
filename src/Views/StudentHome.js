7a4d32bf9f076b6e4d200777e8fb9044e2fd8921
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-16 13:10:40 +0200

    More changes to StudentHome

--- src/Views/StudentHome.js
+++ src/Views/StudentHome.js
@@ -5,7 +5,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import { useState } from 'react';
-import TimeSlot from '../Components/StudentTimeSlot';
+import StudentTimeSlot from '../Components/StudentTimeSlot';
 import Grid from '@mui/material/Grid';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
@@ -13,6 +13,7 @@ import Stack from '@mui/material/Stack';
 import ServicesTabs from '../Components/ServicesTabs';
 import * as LocalStorageManager from '../Utils/LocalStorageManager.js';
 import * as TimeSlotsApiService from '../Services/TimeSlotsApiService.js';
+import * as ReservationsApiService from '../Services/ReservationsApiService.js';
 import dayjs from 'dayjs';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
@@ -37,12 +38,59 @@ export default function StudentHome() {
   const [timeSlotsData, setTimeSlotsData] = useState([]);
 
   // snackbars states:
+  const [successMsg, setSuccessMsg] = useState('0');
+  const [warningMsg, setWarningMsg] = useState('0');
   const [sucessSnackbarOpen, setSucessSnackbarOpen] = useState(false);
   const [warningSnackbarOpen, setWarningSnackbarOpen] = useState(false);
 
   const getSelectedServiceType = (serviceType) => {
     setSelectedServiceType(serviceType);
   };
+
+  function determineServiceType(serviceType) {
+    switch (serviceType) {
+      case 0:
+        return "Laundry";
+
+      case 1:
+        return "Room Cleaning";
+
+      case 2:
+        return "Gym";
+
+      case 3:
+        return "Pool";
+    
+      default:
+        break;
+    }
+  }
+
+  function handleTimeSlotBooking(index) {
+    // find PK_Student and PK_TimeSlot to make the API call:
+    let studentId = authenticatedStudent.PK_KEY;
+    let timeSlotId = timeSlotsData[index].PK_TimeSlot;
+
+    // prepare booking data:
+    let data = {
+      studentId: studentId,
+      timeSlotId: timeSlotId
+    }
+
+    let jsonData = JSON.stringify(data);
+
+    // make api call:
+    ReservationsApiService.addReservation(jsonData, LocalStorageManager.getAuthorizationTokenFromLocalStorage())
+    .then(response => {
+      if (response.status == 200) {
+        setSuccessMsg("Successfully booked the requested time slot!");
+        setSucessSnackbarOpen(true);
+      } else {
+        setWarningMsg("Something went wrong with the booking!");
+        setWarningSnackbarOpen(true);
+      }
+    })
+  }
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -82,12 +130,14 @@ export default function StudentHome() {
                           return response.json();
                         } else {
                           setTimeSlotsData([]);
+                          setWarningMsg('Oops! Could not find any time slots for this date. Try another date!');
                           setWarningSnackbarOpen(true);
                           return Promise.reject();
                         }
                       })
                       .then(json => {
                         setTimeSlotsData(json)
+                        setSuccessMsg('Found Time Slots for the selected date!');
                         setSucessSnackbarOpen(true);
                       })
                       .catch(err => {
@@ -103,11 +153,14 @@ export default function StudentHome() {
                   {
                     timeSlotsData.map((t, index) => (
                       <Grid item xs={6}>
-                        <TimeSlot
+                        <StudentTimeSlot
                           currentCapacity={t.currentCapacity}
                           maxCapacity={t.maximumCapacity}
                           startTime={dayjs(t.startTime).format('hh:mm A')}
                           endTime={dayjs(t.endTime).format('hh:mm A')}
+                          serviceType={determineServiceType(selectedServiceType)}
+                          index={index}
+                          bookingCallback={handleTimeSlotBooking}
                         />
                       </Grid>
                     ))
@@ -123,13 +176,13 @@ export default function StudentHome() {
 
       <Snackbar open={sucessSnackbarOpen} autoHideDuration={3000} onClose={() => setSucessSnackbarOpen(false)}>
         <Alert onClose={() => setSucessSnackbarOpen(false)} severity="success" sx={{ width: '100%' }}>
-          Found Time Slots for the selected date!
+          {successMsg}
         </Alert>
       </Snackbar>
 
       <Snackbar open={warningSnackbarOpen} autoHideDuration={3000} onClose={() => setWarningSnackbarOpen(false)}>
         <Alert onClose={() => setWarningSnackbarOpen(false)} severity="warning" sx={{ width: '100%' }}>
-          Oops! Could not find any time slots for this date. Try another date!
+          {warningMsg}
         </Alert>
       </Snackbar>
 
